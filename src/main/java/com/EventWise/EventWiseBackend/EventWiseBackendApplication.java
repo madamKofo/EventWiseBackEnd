71db214b3c2ffd378ffca888bced62ef6138bd2a
Benita Ezeoke <benita.ezeoke@gmail.com>
2023-05-09 19:28:44 +0300

    first commit

---
+++ src/main/java/com/EventWise/EventWiseBackend/EventWiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.EventWise.EventWiseBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventWiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventWiseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EventWise/EventWiseBackend/EventWiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EventWise.EventWiseBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventWiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
