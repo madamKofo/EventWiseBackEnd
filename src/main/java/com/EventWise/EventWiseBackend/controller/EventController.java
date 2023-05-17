c5eb67b4731d90ab12f08aeff777ce67703dc0ac
stanley ezeoke <stanley.ezeokeh@gmail.com>
2023-05-17 19:56:30 +0300

    added some template& controller

--- src/main/java/com/EventWise/EventWiseBackend/controller/EventController.java
+++ src/main/java/com/EventWise/EventWiseBackend/controller/EventController.java
@@ -1,17 +1,20 @@
 package com.EventWise.EventWiseBackend.controller;
 
 import com.EventWise.EventWiseBackend.DTO.EventDto;
+import com.EventWise.EventWiseBackend.DTO.UserCreateDTO;
 import com.EventWise.EventWiseBackend.service.EventService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@Tag(name = "entity controller", description = "description")
+@Controller
+@RequestMapping("/user")
 public class EventController {
 
     private final EventService eventService;
@@ -21,13 +24,19 @@ public class EventController {
         this.eventService = eventService;
     }
 
-    @GetMapping("/user/{userId}/events")
+    @GetMapping("/{userId}/events")
     public ResponseEntity<List<EventDto>> getAllEventsByUserId(@PathVariable(value = "userId") Long userId){
         return ResponseEntity.ok(eventService.getAllEventsCreatedByUserId(userId));
     }
 
-    @PostMapping("/user/{userId}/events")
-    public ResponseEntity<EventDto> createEvent(@PathVariable Long userId, @RequestBody EventDto eventDto) {
+    @GetMapping("/{userId}/events/create")
+    public String showCreateEvent(@PathVariable(value = "userId") Long userId, Model model) {
+        model.addAttribute("eventDto", new EventDto());
+        return "create-event";
+    }
+
+    @PostMapping("/{userId}/events")
+    public ResponseEntity<EventDto> createEvent(@PathVariable Long userId, @ModelAttribute EventDto eventDto) {
         EventDto createdEvent = eventService.createEvent(userId, eventDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdEvent);
     }
